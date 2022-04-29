de0a54e93811b8a9f3d82b314083bbbe38c6777d
s.motamedi <s.motamedi@rayanbourse.ir>
2022-04-29 15:47:46 +0430

    Add service

---
+++ src/Currency.Application.API/Service/CurrencyService.cs
@@ -0,0 +1,30 @@
+﻿using Didar.Application.API.Entities;
+using Didar.Application.API.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Didar.Application.API.Service
+{
+    public class CurrencyService
+    {
+        private readonly CurrencyBbContext _dbContext;
+
+        public CurrencyService(CurrencyBbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Currency GetCurrencyPrice(CurrencyTypes currencyTypes, DateTime? dateTime)
+        {
+            if (!dateTime.HasValue)
+                dateTime = DateTime.Now;
+            
+            var currency =  _dbContext.Currencies
+                .FirstOrDefault(x => x.CurrencyType == currencyTypes && x.DateTimePrice == dateTime);
+
+            return currency;            
+        }
+    }
+}
