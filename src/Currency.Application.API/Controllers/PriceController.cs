bd4fc39463c624863e00997367ae4168d92314f8
s.motamedi <s.motamedi@rayanbourse.ir>
2022-04-29 15:28:39 +0430

    Add Application.API

---
+++ src/Currency.Application.API/Controllers/PriceController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Didar.Application.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class PriceController : ControllerBase
+    {
+        private readonly DateTime _date;
+
+        public PriceController()
+        {
+            _date = DateTime.Now;
+        }
+
+        [HttpGet(Name =nameof(PriceController))]
+        public int GetPrice(string priceName, DateTime? date)
+        {
+            date = _date;
+
+
+
+            return 1;
+        }
+    }
+}
