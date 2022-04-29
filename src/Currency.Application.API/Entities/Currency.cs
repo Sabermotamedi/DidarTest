787f9bd3508b52fcd479617e497b21a808a66716
s.motamedi <s.motamedi@rayanbourse.ir>
2022-04-29 15:29:48 +0430

    Add Entities

---
+++ src/Currency.Application.API/Entities/Currency.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Didar.Application.API.Entities
+{
+    public class Currency
+    {
+        public int Id { get; set; }
+        public CurrencyTypes CurrencyType { get; set; }
+        public DateTime DateTimePrice { get; set; }
+    }
+}
---
+++ src/Currency.Application.API/Entities/CurrencyType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Didar.Application.API.Entities
+{
+    public enum CurrencyTypes
+    {
+        USD = 0,
+        EUR = 1,
+        UAE = 2,
+        MYR = 3
+    }
+}
