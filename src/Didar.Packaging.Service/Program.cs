5b3e60ded5c0e48358536a4e4f800028ed79a5df
s.motamedi <s.motamedi@rayanbourse.ir>
2022-04-30 18:27:40 +0430

    Replace Packaging service

--- src/Didar.Packaging.Service/Program.cs
+++ src/Didar.Packaging.Service/Program.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Didar.Packaging.API
+namespace Didar.Packaging.Service
 {
     public class Program
     {
